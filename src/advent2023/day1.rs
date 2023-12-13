f54399b4082e2a7e2e3515cb961325a093bd4e31
Anthony M. Bonafide <anthony.bonafide@shipt.com>
2023-12-13 09:50:54 -0500

    Project Layout and start to day 1 solution
    Signed-off-by: Anthony M. Bonafide <anthony.bonafide@shipt.com>

---
+++ src/advent2023/day1.rs
@@ -0,0 +1,5 @@
+fn trebuchet(input : String) -> i32 {
+    let input = crate::get_day_input(1);
+
+}
+
---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use std::fs;
+
+mod advent2023;
+
+
+fn get_day_input(day_number: u8) -> String {
+    fs::read_to_string(format!("./test/day{day_number}_input.txt")).expect("data")
+}
+
+#[cfg(test)]
+mod test {
+    use crate::get_day_input;
+
+    #[test]
+    fn test_day_1(){
+        let input = get_day_input(1);
+        assert_eq!(input.is_empty(), false)
+    }
+}
